d2e7e5b640286ddc78bb1127ade01fa3147bd5b0
minsler <minsler.by@gmail.com>
2014-03-30 01:56:36 +0300

    Added sum method to Calculator. Added javadoc to Calculator.

--- src/main/java/by/minsler/Calculator.java
+++ src/main/java/by/minsler/Calculator.java
@@ -1,8 +1,31 @@
 package by.minsler;
 
+/**
+ * Class to get sqrt and sum.
+ *
+ * @author Dmitry Vorobey
+ */
 public class Calculator {
 
-    public static double sqrt(int x){
+    /**
+     * Returns sqrt for the input number.
+     *
+     * @param x input number to get sqrt from
+     *
+     * @return sqrt for input number
+     */
+    public static double sqrt(int x) {
         return Math.sqrt(x);
+    }
+
+    /**
+     * Returns sum for input numbers.
+     *
+     * @param a the first number
+     * @param b the second number
+     * @return sum of input numbers
+     */
+    public static double sum(double a, double b) {
+        return a + b;
     }
 }
--- src/test/java/by/minsler/TestCalculator.java
+++ src/test/java/by/minsler/TestCalculator.java
@@ -4,8 +4,14 @@ import org.junit.Test;
 import org.junit.Assert;
 
 public class TestCalculator {
+
     @Test
     public void testSqrt(){
         Assert.assertEquals(7, Calculator.sqrt(49), 0.0001);
+    }
+
+    @Test
+    public void testSum(){
+        Assert.assertEquals(30, Calculator.sum(10, 20), 0.1);
     }
 }
