44b72ddabef5c0af0e8297c58eb8fdffd59296b6
DeMiTeLi <dmitrii1790@gmail.com>
2014-03-29 23:20:29 +0200

    Fixes errors for normally test passing

--- src/test/java/by/minsler/TestCalculator.java
+++ src/test/java/by/minsler/TestCalculator.java
@@ -5,7 +5,7 @@ import org.junit.Assert;
 
 public class TestCalculator {
     @Test
-    public static void testSqrt(){
+    public void testSqrt(){
         Assert.assertEquals(7, Calculator.sqrt(49), 0.0001);
     }
 }
