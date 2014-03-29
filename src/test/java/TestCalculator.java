22a51fc6223748b923cab740cea5f62faa468ea1
DeMiTeLi <dmitrii1790@gmail.com>
2014-03-29 02:12:25 +0200

    First Project and TestClass

---
+++ src/test/java/TestCalculator.java
@@ -0,0 +1,12 @@
+import by.minsler.*;
+import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Ignore;
+
+public class TestCalculator extends TestCase {
+      public static void testSqrt(){
+        Assert.assertEquals(7, Calculator.sqrt(49), 0 );
+    }
+
+    }
+
